2ee9e47ff8c2c970bc2170ac1479577cea2e6b10
Adam Mueller <adam@stembolt.com>
2017-12-01 14:57:32 -0800

    Refactor!
    I come from the net. Through systems, peoples and cities to this place:
    Mainframe. My format: Guardian, to mend and defend. To defend my new
    found friends, their hopes and dreams. To defend them from their
    enemies. They say the user lives outside the net, and inputs games for
    pleasure. No one knows for sure, but I intend to find out.

--- 2017/01/solution.rb
+++ 2017/01/solution.rb
@@ -1,14 +1,7 @@
 class Solution
-  def self.part1(x)
+  def self.call(x, s = 1)
     x.each_char.with_index.map do |c, i|
-      c == x[(i + 1) % x.length] ? c.to_i : 0
-    end.sum
-  end
-
-  def self.part2(x)
-    steps = x.length / 2
-    x.each_char.with_index.map do |c, i|
-      c == x[(i + steps) % x.length] ? c.to_i : 0
+      c == x[(i + s) % x.length] ? c.to_i : 0
     end.sum
   end
 end
--- 2017/01/test.rb
+++ 2017/01/test.rb
@@ -3,38 +3,38 @@ require_relative "solution"
 
 class SolutionTest < Minitest::Test
   def test_simple_part1
-    assert Solution.part1("1122") == 3
+    assert Solution.("1122") == 3
   end
 
   def test_all_matches_part1
-    assert Solution.part1("1111") == 4
+    assert Solution.("1111") == 4
   end
 
   def test_no_matches_part1
-    assert Solution.part1("1234") == 0
+    assert Solution.("1234") == 0
   end
 
   def test_end_matches_part1
-    assert Solution.part1("91212129") == 9
+    assert Solution.("91212129") == 9
   end
 
   def test_simple_part2
-    assert Solution.part2("1212") == 6
+    assert Solution.("1212", 2) == 6
   end
 
   def test_no_matches_part2
-    assert Solution.part2("1221") == 0
+    assert Solution.("1221", 2) == 0
   end
 
   def test_one_match_part2
-    assert Solution.part2("123425") == 4
+    assert Solution.("123425", 3) == 4
   end
 
   def test_all_match_part2
-    assert Solution.part2("123123") == 12
+    assert Solution.("123123", 3) == 12
   end
 
   def test_complex_part2
-    assert Solution.part2("12131415") == 4
+    assert Solution.("12131415", 4) == 4
   end
 end
