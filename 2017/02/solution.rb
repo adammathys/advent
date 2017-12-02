05c50d1f85ff6e7c6371bd967a6ce199bd7db9ae
Adam Mueller <adam@stembolt.com>
2017-12-01 21:56:43 -0800

    Clean-up second solution a tiny bit

--- 2017/02/solution.rb
+++ 2017/02/solution.rb
@@ -1,17 +1,16 @@
 class Solution
   class << self
     def firstStar(input)
-      input.split("\n").map do |line|
+      input.lines.map do |line|
         numbers = line.split.map(&:to_i)
         numbers.max - numbers.min
       end.sum
     end
 
     def secondStar(input)
-      input.split("\n").flat_map do |line|
+      input.lines.flat_map do |line|
         n = line.split.map(&:to_i)
-        n.flat_map do |x|
-          (n - [x]).map { |y| division(x, y) }
+        n.flat_map { |x| (n - [x]).map { |y| division(x, y) } }
         end
       end.sum
     end
@@ -19,7 +18,7 @@ class Solution
     private
 
     def division(x, y)
-      (x > y) && (x % y == 0) ? x/y : 0
+      (x % y == 0) ? x/y : 0
     end
   end
 end
