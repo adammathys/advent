13e09d05826943eccc5e4e5cb38acc321417fcac
Adam Mueller <adam@stembolt.com>
2019-12-04 15:52:11 -0800

    Use stdlib Vector for day 3
    Always forget this exists. Need to stop trying to write my own. :(

--- 2019/03/circuit.rb
+++ 2019/03/circuit.rb
@@ -1,12 +1,14 @@
+require "matrix"
+
 class Circuit
   attr_reader :paths
 
   def initialize(paths)
-    @paths = paths
+    @paths = paths.map{ |p| p.split(",") }
   end
 
   def closest_intersection
-    intersections.keys.map { |x| distance(x, [0,0]) }.min
+    intersections.keys.map { |x| distance(x) }.min
   end
 
   def fewest_steps
@@ -15,30 +17,25 @@ class Circuit
 
   private
 
-  def distance(a, b)
-    (a[0] - b[0]).abs + (a[1] - b[1]).abs
+  def distance(a, b = Vector[0,0])
+    (a - b).to_a.map(&:abs).sum
   end
 
   def intersections
     visited_points =
-      @paths.map do |path|
+      @paths.map do |steps|
         points = {}
-        current = [0,0]
+        current = Vector[0,0]
 
-        path.split(",").each do |step|
+        steps.each do |step|
           direction, magnitude = parse_instruction(step)
           steps = points[current] || 0
+
           magnitude.times do
-            new_location =
-              [
-                current[0] += direction[0],
-                current[1] += direction[1],
-              ]
+            current += direction
             steps += 1
 
-            unless points.key?(new_location)
-              points[new_location] = steps
-            end
+            points[current] ||= steps
           end
         end
 
@@ -46,22 +43,20 @@ class Circuit
       end
 
     keys = visited_points.map(&:keys).reduce(:&)
-    keys.map do |x|
-      [x, visited_points.sum{ |y| y[x] }]
-    end.to_h
+    keys.map { |x| [x, visited_points.sum{ |y| y[x] }] }.to_h
   end
 
   def parse_instruction(instruction)
     direction =
       case instruction[0]
       when 'R'
-        [1,0]
+        Vector[1,0]
       when 'D'
-        [0,-1]
+        Vector[0,-1]
       when 'L'
-        [-1,0]
+        Vector[-1,0]
       when 'U'
-        [0,1]
+        Vector[0,1]
       end
 
     magnitude = instruction[1..-1].to_i
