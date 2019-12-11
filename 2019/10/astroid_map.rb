a8f49af506f1a56a3f721ce798b03d44988916b1
Adam Mueller <adam.mathys@gmail.com>
2019-12-11 11:47:38 -0800

    Day 10: Finish part 2
    Took me a while to wrap my head around the angle math, wasn't nearly as
    hard as I was making it out my mind. Really just needed to sleep on it.
    
    Kind of happy with how the solution turned out. Definitely not optimized
    for performance, but I think it's reasonably easy to follow the logic.

--- 2019/10/astroid_map.rb
+++ 2019/10/astroid_map.rb
@@ -1,5 +1,3 @@
-require "pry"
-
 class AstroidMap
   attr_reader :width, :height, :image
 
@@ -33,8 +31,23 @@ class AstroidMap
     grouped = astroids.group_by do |x2, y2|
       angle(vaporizer[0], vaporizer[1], x2, y2)
     end
+    grouped.each do |angle, positions|
+      grouped[angle] = sorted(positions, vaporizer)
+    end
 
-    binding.pry
+    angles = rearrange(grouped.keys)
+
+    vaporized = 0
+    last_vaporized = nil
+
+    while vaporized < n
+      angle = angles[vaporized % angles.length]
+
+      last_vaporized = grouped[angle].shift
+      vaporized += 1 if last_vaporized
+    end
+
+    last_vaporized
   end
 
   private
@@ -63,7 +76,42 @@ class AstroidMap
       end.compact
   end
 
+  # Sort angles clockwise starting with "up."
+  def rearrange(angles)
+    sorted = []
+
+    # up -> right
+    top_right = angles.select { |a| a <= (Math::PI / 2) && a >= 0 }
+    sorted.push(*top_right.sort.reverse)
+
+    # right -> down
+    bottom_right = angles.select { |a| a < 0 && a >= (Math::PI / -2) }
+    sorted.push(*bottom_right.sort.reverse)
+
+    # down -> left
+    bottom_left = angles.select { |a| a < (Math::PI / -2) && a >= -Math::PI }
+    sorted.push(*bottom_left.sort.reverse)
+
+    # left -> up
+    top_left = angles.select { |a| a <= Math::PI && a > (Math::PI / 2) }
+    sorted.push(*top_left.sort.reverse)
+
+    sorted
+  end
+
+  # Sort positions from closest to farthest from origin.
+  def sorted(positions, origin)
+    positions.sort_by do |position|
+      distance(position, origin)
+    end
+  end
+
+  # Using manhattan for simplicity.
+  def distance(a, b)
+    (a[0] - b[0]).abs + (a[1] - b[1]).abs
+  end
+
   def angle(x, y, x2, y2)
-    Math.atan2(y2 - y, x2 - x)
+    Math.atan2(y - y2, x2 - x)
   end
 end
--- 2019/10/run.rb
+++ 2019/10/run.rb
@@ -6,3 +6,7 @@ astroid_map = AstroidMap.new(lines)
 
 x, y = astroid_map.best_location
 puts astroid_map.num_detected(x, y)
+
+vaporized = astroid_map.nth_vaporized(200)
+puts vaporized.join(", ")
+puts vaporized[0] * 100 + vaporized[1]
